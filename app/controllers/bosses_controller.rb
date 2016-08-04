65378a4f8a2561daa12e5734e6f927ff36d5ff5b
StevenGreenup <greenup.steven@gmail.com>
2016-08-04 16:31:38 -0500

    styled company profile page and added twitter to it also styled the boss page

--- app/controllers/bosses_controller.rb
+++ app/controllers/bosses_controller.rb
@@ -21,56 +21,57 @@ def create
 
   @boss.user_id = @current_user.id
   unless response.nil?
+
   @boss.name = response.person.name.fullName
 
-unless response.person.twitter.nil?
-  @boss.twitter = response.person.twitter.handle
-  @boss.twitter_bio = response.person.twitter.bio
-  @boss.twitter_followers = response.person.twitter.followers
-  @boss.twitter_following = response.person.twitter.following
-  @boss.twitter_site = response.person.twitter.site
-  @boss.twitter_avatar = response.person.twitter.avatar
-end
-  @boss.linked = response.person.linkedin.handle unless response.person.linkedin.nil?
-  @boss.company = response.person.employment.name unless response.person.company.nil?
-  @boss.gender = response.person.gender unless response.person.nil?
-  @boss.location = response.person.location unless response.person.nil?
-  @boss.facebook = response.person.facebook.handle unless response.person.facebook.nil?
-unless response.person.github.nil?
-  @boss.github_handle = response.person.github.handle
-  @boss.github_avatar = response.person.github.avatar
-  @boss.github_company = response.person.github.company
-  @boss.github_blog = response.person.github.blog
-  @boss.github_followers = response.person.github.followers
-  @boss.github_following = response.person.github.following
-end
-  @boss.google = response.person.googleplus.handle unless response.person.googleplus.nil?
-unless response.person.angellist.nil?
-  @boss.angellist = response.person.angellist.handle
-  @boss.angel_bio = response.person.angellist.bio
-  @boss.angel_blog = response.person.angellist.blog
-  @boss.angel_avatar = response.person.angellist.avatar
-end
-unless response.person.aboutme.nil?
-  @boss.about_handle = response.person.aboutme.handle
-  @boss.about_bio = response.person.aboutme.bio
-  @boss.about_avatar = response.person.aboutme.avatar
-end
-unless response.person.gravatar.nil?
-  @boss.gravatar = response.person.gravatar.handle
-  @boss.gravatar_avatar = response.person.gravatar.avatar
-end
-end
-  if @boss.save
-    redirect_to user_path(id: @current_user.id)
-  else
+    unless response.person.twitter.nil?
+      @boss.twitter = response.person.twitter.handle
+      @boss.twitter_bio = response.person.twitter.bio
+      @boss.twitter_followers = response.person.twitter.followers
+      @boss.twitter_following = response.person.twitter.following
+      @boss.twitter_site = response.person.twitter.site
+      @boss.twitter_avatar = response.person.twitter.avatar
+    end
+      @boss.linked = response.person.linkedin.handle unless response.person.linkedin.nil?
+      @boss.company = response.person.employment.name unless response.person.company.nil?
+      @boss.gender = response.person.gender unless response.person.nil?
+      @boss.location = response.person.location unless response.person.nil?
+      @boss.facebook = response.person.facebook.handle unless response.person.facebook.nil?
+    unless response.person.github.nil?
+      @boss.github_handle = response.person.github.handle
+      @boss.github_avatar = response.person.github.avatar
+      @boss.github_company = response.person.github.company
+      @boss.github_blog = response.person.github.blog
+      @boss.github_followers = response.person.github.followers
+      @boss.github_following = response.person.github.following
+    end
+      @boss.google = response.person.googleplus.handle unless response.person.googleplus.nil?
+    unless response.person.angellist.nil?
+      @boss.angellist = response.person.angellist.handle
+      @boss.angel_bio = response.person.angellist.bio
+      @boss.angel_blog = response.person.angellist.blog
+      @boss.angel_avatar = response.person.angellist.avatar
+    end
+    unless response.person.aboutme.nil?
+      @boss.about_handle = response.person.aboutme.handle
+      @boss.about_bio = response.person.aboutme.bio
+      @boss.about_avatar = response.person.aboutme.avatar
+    end
+    unless response.person.gravatar.nil?
+      @boss.gravatar = response.person.gravatar.handle
+      @boss.gravatar_avatar = response.person.gravatar.avatar
+    end
+  end
+      if @boss.save
+        redirect_to user_path(id: @current_user.id)
+      else
 
 
-    redirect_to user_path(id: @current_user.id), notice: "Hiring manager could not be found"
-
+    redirect_to root_path, notice: "Hiring manager could not be found"
+    end
   end
 
-end
+
 
 
 def update
