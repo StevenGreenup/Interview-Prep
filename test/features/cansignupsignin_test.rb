af722232d07d027428d5ef7e50125f920eac923c
StevenGreenup <greenup.steven@gmail.com>
2016-08-11 10:40:19 -0500

    added tests

--- test/features/cansignupsignin_test.rb
+++ test/features/cansignupsignin_test.rb
@@ -1,20 +1,23 @@
 require "test_helper"
-db:environment:set RAILS_ENV=test
 
 class CansignupsigninTest < Capybara::Rails::TestCase
   test "Users can sign in after signing up" do
     visit root_path
-    fill_in "Name", with: "testing"
-    fill_in "Email", with: "test@test.com"
-    fill_in "Password", with: "testing"
-    fill_in "Password confirmation", with: "testing"
+    assert_content "Sign up"
+
+    fill_in "user[name]", with: "testing"
+    fill_in "user[email]", with: "test@test.com"
+    fill_in "user[password]", with: "testing"
+    fill_in "user[password_confirmation]", with: "testing"
     click_button "Sign up"
     assert_content "Companies & Hiring Managers"
     click_link "Sign Out"
-    click_link "Sign in"
-    fill_in "Email", with: "testing"
-    fill_in "Password", with: "testing"
-    click_button "Sign in"
+    save_and_open_page
+    within("div.sign-in") do
+      fill_in "Email", with: "test@test.com"
+      fill_in "Password", with: "testing"
+      click_button "Sign in"
+    end
     assert_content "Companies & Hiring Managers"
   end
 end
